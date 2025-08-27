a9028aa94971ac20eeb45f9a516247a956fddb5e
Maria Clara Dantas <Mariaclaradtorres@gmail.com>
2025-08-26 23:31:26 -0300

    Time: 131 ms (26.47%) | Memory: 13.1 MB (54.56%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,8 +1,5 @@
 class Solution:
     def isPalindrome(self, s):
-        if len(s) == 0 or len(s) == 1:
-            return True
-
         a = ""
         s = s.lower()
         al = "1234567890qwertyuiopasdfghjklzxcvbnm"
@@ -11,13 +8,10 @@ class Solution:
             if i in al:
                 a += i 
         
-        i = 0
-        j = len(a) - 1
+        return a == a[::-1]
 
-        while i < j:
-            if a[i] != a[j]:
-                return False
-            i += 1
-            j -= 1
+
+        
+        
 
         return True
