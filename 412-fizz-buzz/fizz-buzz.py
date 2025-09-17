89f99d2cc1af5ba02bd8fa29c495215b0cd8b869
Maria Clara Dantas <Mariaclaradtorres@gmail.com>
2025-09-17 14:52:46 -0300

    Time: 2 ms (49.26%) | Memory: 13.7 MB (6.09%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        out = []
+        for i in range(n):
+            out.append(i + 1)
+        for i in range(n):
+            if out[i] % 3 == 0 and out[i] % 5 == 0:
+                out[i] = "FizzBuzz"
+            elif out[i] % 3 == 0:
+                out[i] = "Fizz"
+            elif out[i] % 5 == 0:
+                out[i] = "Buzz"
+            else:
+                out[i] = str(out[i])  # garante que tudo vira string
+        return out
