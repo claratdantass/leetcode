07ea3809b9e672eaecc1ca2d8117784b5bb638ca
Maria Clara Dantas <Mariaclaradtorres@gmail.com>
2025-09-17 14:53:53 -0300

    Time: 3 ms (39.34%) | Memory: 13.5 MB (6.09%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.py
+++ 412-fizz-buzz/fizz-buzz.py
@@ -1,15 +1,13 @@
 class Solution(object):
     def fizzBuzz(self, n):
         out = []
-        for i in range(n):
-            out.append(i + 1)
-        for i in range(n):
-            if out[i] % 3 == 0 and out[i] % 5 == 0:
-                out[i] = "FizzBuzz"
-            elif out[i] % 3 == 0:
-                out[i] = "Fizz"
-            elif out[i] % 5 == 0:
-                out[i] = "Buzz"
+        for i in range(1, n + 1):
+            if i % 3 == 0 and i % 5 == 0:
+                out.append("FizzBuzz")
+            elif i % 3 == 0:
+                out.append("Fizz")
+            elif i % 5 == 0:
+                out.append("Buzz")
             else:
-                out[i] = str(out[i])  # garante que tudo vira string
+                out.append(str(i))
         return out
